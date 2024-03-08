4200dded9caf075b5c97e6796bac419cda106401
lalalion <2672806705@qq.com>
2024-03-09 02:41:37 +0800

    二级分类所属分类的下拉框数据

--- routes/category.ts
+++ routes/category.ts
@@ -4,8 +4,8 @@ import { Category } from "../model";
 var router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const { name, level, pageSize, current } = req.query;
-
+  const { name, level, all, pageSize, current } = req.query;
+  /*原写法产生bug,c创建分类时选择二级分类出现所属分类的下拉框时，不能返回所有一级分类的名称
   const total = await Category.countDocuments(req.body);
 
   const data = await Category.find({
@@ -16,6 +16,39 @@ router.get("/", async (req: Request, res: Response) => {
     .sort({ updatedAt: -1 })
     .populate("parent");
   return res.status(200).json({ data, success: true, total });
+  */
+
+  if (all === 'true') {
+    try {
+      const data = await Category.find({
+        ...(name && { name }),
+        ...(level && { level }),
+      })
+        .sort({ updatedAt: -1 })
+        .populate("parent");
+      return res.status(200).json({ data, total: data.length });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+  try {
+    const total = await Category.countDocuments({
+      ...(name && { name }),
+      ...(level && { level }),
+    });
+    const data = await Category.find({
+      ...(name && { name }),
+      ...(level && { level }),
+    })
+      .skip((Number(current) - 1) * Number(pageSize))
+      .sort({ updatedAt: -1 })
+      .populate("parent");
+    return res.status(200).json({ data, success: true, total });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 router.post("/", async (req: Request, res: Response) => {
