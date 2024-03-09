2bd9da7500006914b16aeeee377ed50545042acd
lalalion <2672806705@qq.com>
2024-03-09 16:02:52 +0800

    修改代理

--- app.ts
+++ app.ts
@@ -58,7 +58,7 @@ app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(3001, () => {
+app.listen('3001', () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${3001}`);
 });
 
