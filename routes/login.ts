6083c1103ffce5ec5b7c185d834c085675f7ffaa
lalalion <2672806705@qq.com>
2024-04-04 17:57:13 +0800

    账号存在但密码错误情况处理

--- routes/login.ts
+++ routes/login.ts
@@ -6,23 +6,29 @@ router.post('/', async (req: Request, res: Response) => {
   const { name, password } = req.body;
   try {
     // 查询数据库是否存在该用户
-    const user = await User.findOne({ name, password });
+    const user = await User.findOne({ name });
 
     // 如果用户不存在，则返回相应提示信息
     if (!user) {
       return res.status(404).json({ message: '用户未注册' });
     }
 
-    // 用户存在，则继续处理登录逻辑
-    const data = user?.toJSON();
-    delete data?.password;
+    // 如果用户存在，则进行密码校验
+    if (user.password !== password) {
+      return res.status(500).json({ message: '密码错误' });
+    }
 
-    (req.session as any).user = user;
+    // 密码校验通过，则登录成功
+    const userData = user.toJSON();
+    delete userData.password;
 
-    return res.status(200).json({ data, success: true });
+    (req.session as any).user = userData;
+
+    return res.status(200).json({ data: userData, success: true });
   } catch (error) {
-    return res.status(500).json({ message: '账号密码错误' });
+    return res.status(500).json({ message: '服务器内部错误' });
   }
 });
+
 
 export default router;
